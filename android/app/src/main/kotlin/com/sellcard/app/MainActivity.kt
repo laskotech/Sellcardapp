ea484e3b04d525c19f280a1e9689cd2924143de2
laskotech <laskoforex1@gmail.com>
2025-08-21 03:30:39 +0100

    migrated main activity to flutter v2 embadding

--- android/app/src/main/kotlin/com/sellcard/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/sellcard/app/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.sellcard.app
+package com.sellcard.app  // keep this line the same as your folder/package name
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {}
+class MainActivity: FlutterActivity() {
+}
